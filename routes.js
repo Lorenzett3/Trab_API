a4921f515f3bdd664e49d8eba359f5f9800e4ac8
Lorenzo <lorenzomoraes0901@gmail.com>
2025-10-28 18:51:10 -0300

    feat: auth jwt

--- routes.js
+++ routes.js
@@ -1,17 +1,24 @@
+// routes.js
+
 const express = require('express');
 const router = express.Router();
 const controller = require('./controllers');
+const authController = require('./authController');
+const { verifyToken } = require('./middlewares/auth');
+
+router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 router.get('/tiposclientes', controller.listTiposClientes);
 router.get('/tiposclientes/:id', controller.getTipoClienteById);
 
-router.post('/tiposclientes', controller.validateTipoCliente, controller.createTipoCliente);
-router.put('/tiposclientes/:id', controller.validateTipoCliente, controller.updateTipoCliente);
-router.delete('/tiposclientes/:id', controller.deleteTipoCliente);
+router.post('/tiposclientes', verifyToken, controller.validateTipoCliente, controller.createTipoCliente);
+router.put('/tiposclientes/:id', verifyToken, controller.validateTipoCliente, controller.updateTipoCliente);
+router.delete('/tiposclientes/:id', verifyToken, controller.deleteTipoCliente);
 
-router.post('/clientes', controller.createCliente);
+router.post('/clientes', verifyToken, controller.createCliente);
 
-router.post('/emprestimos', controller.realizarEmprestimo);
-router.post('/devolucoes', controller.realizarDevolucao);
+router.post('/emprestimos', verifyToken, controller.realizarEmprestimo);
+router.post('/devolucoes', verifyToken, controller.realizarDevolucao);
 
 module.exports = router;
