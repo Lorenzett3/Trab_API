6583b54826bf578d695b514425ff625d8d3dd269
LORENZO DE OLIVEIRA MORAES <85558966068@senacrs.edu.br>
2025-12-02 20:10:15 -0300

    fix: limpando o terreno para o merge

--- Servicos-e-APIs-Trab-1/router/emprestimo_router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const emprestimoController = require("../controller/emprestimo_controller");
-
-router.post('/emprestimos', emprestimoController.inserir);
-
-module.exports = router;
